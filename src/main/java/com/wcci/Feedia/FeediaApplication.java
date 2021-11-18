50ee28b7956c00609405581f8e731b98ebd207cd
Josh Wright <joshnotwright@gmail.com>
2021-11-18 11:49:05 -0500

    feat: adds spring init

---
+++ src/main/java/com/wcci/Feedia/FeediaApplication.java
@@ -0,0 +1,13 @@
+package com.wcci.Feedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeediaApplication.class, args);
+	}
+
+}

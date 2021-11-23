db539167b72ebeb3242a972bac97f7b986e63793
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2021-11-23 13:30:42 -0500

    it's a process

---
+++ src/main/resources/static/javascript/app.js
@@ -0,0 +1,32 @@
+import {displayPetView} from "./pet.js";
+
+const conteainerEl = document.querySelector(".container");
+
+buildHeader();
+
+function clearChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.lastChild);
+    }
+};
+
+function buildHeader() {
+    const sectionEl = document.createElement("section");
+    const divEl = document.createElement("div");
+    divEl.classList("site_links");
+
+    const headerEl = document.createElement("header");
+    headerEl.id = "header";
+
+    const headerLogoEl = document.createElement("img");
+    headerLogoEl.src= "images/feedia_logo.png";
+    headerLogoEl.alt = "site logo";
+    headerLogoEl.classList.add("logo");
+
+    sectionEl.appendChild(headerEl);
+    headerEl.appendChild(headerLogoEl);
+    conteainerEl.appendChild(headerEl);
+
+
+
+}

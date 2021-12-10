503cadb2c237c2f3f64f186fc7e3e4adee732555
evanboylan <65260914+evanboylan@users.noreply.github.com>
2021-12-10 16:14:34 -0500

    Updated populator entries

--- src/main/java/com/wcci/Feedia/Populator.java
+++ src/main/java/com/wcci/Feedia/Populator.java
@@ -28,28 +28,28 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Reptile reptile1 = new Reptile("Kaiju", "Corn Snake", 4, "Male","../images/kaiju.png", "Brown body, bad temper", 0f, 0f);
+        Reptile reptile1 = new Reptile("Kaiju", "Gecko", 4, "Male","../images/kaiju.png", "Well-behaved, likes to eat.", 0f, 0f);
         reptileRepo.save(reptile1);
         reptile1.createCalendar();
-        reptile1.myCalendar.createEvent("Feed Yo Lizzzzard", "My Lizard's House", "I've gotta remember to feed this guy",
+        reptile1.myCalendar.createEvent("Feed Kaiju", "Guest Room", "Five crickets, kale, and tomatoes.",
                 "2021-12-08T12:30:00.000-05:00", "2021-12-08T12:45:00.000-05:00", "WEEKLY", "50");
         reptile1.myCalendar.setReptile(reptile1);
         googleCalendarRepo.save(reptile1.getMyCalendar());
 
 
-        Reptile reptile2 = new Reptile("Cayde", "Leopard Gecko", 6, "Male","../images/cayde.png", "He don't like being touched", 0f, 0f);
+        Reptile reptile2 = new Reptile("Cayde", "Leopard Gecko", 6, "Male","../images/cayde.png", "Loves to perch, eat anything that moves.", 0f, 0f);
         reptileRepo.save(reptile2);
         reptile2.createCalendar();
         reptile2.myCalendar.setReptile(reptile2);
         googleCalendarRepo.save(reptile2.getMyCalendar());
 
-        Reptile reptile3 = new Reptile("Ginger", "Leopard Gecko", 3, "Female","../images/ginger.jpg", "Loves to eat anything", 0f, 0f);
+        Reptile reptile3 = new Reptile("Ginger", "Leopard Gecko", 3, "Female","../images/ginger.jpg", "Has a big appetite, likes vegetables.", 0f, 0f);
         reptileRepo.save(reptile3);
         reptile3.createCalendar();
         reptile3.myCalendar.setReptile(reptile3);
         googleCalendarRepo.save(reptile3.getMyCalendar());
 
-        Reptile reptile4 = new Reptile("Cosmo", "Chameleon", 4, "Male","../images/cosmo.jpg", "Likes staring at the wall", 0f, 0f);
+        Reptile reptile4 = new Reptile("Cosmo", "Chameleon", 4, "Male","../images/cosmo.jpg", "Likes to relax, lounge anywhere available.", 0f, 0f);
         reptileRepo.save(reptile4);
         reptile4.createCalendar();
         reptile4.myCalendar.setReptile(reptile4);
@@ -58,27 +58,27 @@ public class Populator implements CommandLineRunner {
         Need need1 = new Need("Medicine Twice Daily", reptile1);
         needRepo.save(need1);
 
-        Note note1 = new Note("Observation:", "Stepped in its own poop on 11/22/2021 at 11:47 AM", true, reptile1);
+        Note note1 = new Note("Observation:", "Seems to prefer mealworms to silkworms.", true, reptile1);
         noteRepo.save(note1);
 //        note1.setReptile(reptile1);
 //        reptileRepo.save(reptile1);
 
-        Note note2 = new Note("Observation:", "Tried to eat newspaper.", true, reptile1);
+        Note note2 = new Note("Observation:", "They seemed to love the waxworm but should cut those back to once a week.", true, reptile2);
         noteRepo.save(note2);
 //        note2.setReptile(reptile2);
 //        reptileRepo.save(reptile2);
 
-        Note note3 = new Note("Observation:", "Tried to climb up the glass again.", true, reptile1);
+        Note note3 = new Note("Observation:", "Noticed they really seemed active today.", true, reptile3);
         noteRepo.save(note3);
 //        note3.setReptile(reptile3);
 //        reptileRepo.save(reptile3);
 
-        Note note4 = new Note("Observation:", "Couldn't find a few crickets.", true, reptile1);
+        Note note4 = new Note("Observation:", "Noticed they eat orange foods like squash before green.", true, reptile4);
         noteRepo.save(note4);
 //        note4.setReptile(reptile4);
 //        reptileRepo.save(reptile4);
 
-        Schedule schedule1 = new Schedule("Poop Laying", "Needs to go twice daily", 12, reptile1);
+        Schedule schedule1 = new Schedule("Kaiju's Schedule", "Schedule for Feeding", 12, reptile1);
         scheduleRepo.save(schedule1);
 
         
